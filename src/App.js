4eba4bef8c7ee8a7430b296843a175e995aaf5cd
mukul98s <mymukul112@gmail.com>
2021-05-22 20:02:54 +0530

    feat: Added Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
