d56c814f3e6e3dfd2e2e290c569a238eef3fb8c3
mukul98s <mymukul112@gmail.com>
2021-05-23 16:05:10 +0530

    feat: login page completed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import login from "../images/login.svg";
+import styled from "styled-components";
 
 function Login() {
   return (
-    <div>
-      <h1>login</h1>
+    <div className="container">
+      <Wrapper>
+        <div>
+          <img src={login} alt="Error 404" />
+        </div>
+        <Link to="" className="button">
+          Login
+        </Link>
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  overflow: hidden;
+  flex-direction: column;
+  div {
+    width: 40vw;
+    text-align: center;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export default Login;
