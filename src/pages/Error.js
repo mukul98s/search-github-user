094d277e1ff412568665a803568f52f0a583645a
mukul98s <mymukul112@gmail.com>
2021-05-23 16:05:27 +0530

    feat: error page completed

---
+++ src/pages/Error.js
@@ -0,0 +1,38 @@
+import React from "react";
+import error from "../images/error.svg";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+function Error() {
+  return (
+    <Wrapper>
+      <div>
+        <img src={error} alt="Error 404" />
+      </div>
+      <Link to="/" className="button">
+        Back To Home
+      </Link>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  max-height: 100vh;
+  overflow: hidden;
+  flex-direction: column;
+  div {
+    width: 50vw;
+    text-align: center;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    margin-bottom: 40px;
+  }
+`;
+
+export default Error;
