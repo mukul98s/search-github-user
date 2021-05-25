ca24b8077f33ab03d55fa0b23da297e5ac74c02a
mukul98s <mymukul112@gmail.com>
2021-05-25 20:21:43 +0530

    feat: AuthProvider Render

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { GlobalProvider } from "./context/context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalProvider>
-      <App />
-    </GlobalProvider>
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENT_ID}
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
